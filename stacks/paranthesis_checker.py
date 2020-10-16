d83023986dac2ebc95e76f17312de4be30c0d77c
aditigupta16 <gaditi80@gmail.com>
2020-10-16 21:40:51 +0530

    paranthesis checker

---
+++ stacks/paranthesis_checker.py
@@ -0,0 +1,22 @@
+
+
+def paranthesis_check(s):
+    if len(s) % 2:
+        return False
+    paranthesis_mapping = {']': '[', '}': '{', ')': '('}
+    stack = []
+
+    for bracket in s:
+        if str(bracket) not in paranthesis_mapping:
+            stack.append(bracket)
+        else:
+            if not stack or stack.pop()!=paranthesis_mapping[bracket]:
+                return False
+    if stack > 0:
+        return False
+    return True
+
+
+if __name__ == '__main__':
+    s = '){})'
+    print(paranthesis_check(s))
